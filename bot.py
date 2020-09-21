e75b216f46056e1aaad57eac4cdca43f3ebc1f9e
40282125 <jbrown88@qub.ac.uk>
2020-09-21 09:53:09 +0100

    Added command to allow user to edit prefix.

--- bot.py
+++ bot.py
@@ -30,6 +30,28 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    if message.content.startswith(PREFIX) and message.content[1:].lower() in help_commands:
+        commands_response = "Here are a list of my commands:"
+        await message.channel.send(commands_response)
+
+    if message.content == PREFIX + 'prefix':
+        if message.author.guild_permissions.administrator:
+            new_prefix = await change_prefix(message)
+            PREFIX = new_prefix
+        else:
+            no_permissions = "You don't have the permissions for this."
+            await message.channel.send(no_permissions)
+
+
+async def change_prefix(message):
+    prompt_user = "Please enter a new prefix."
+    await message.channel.send(prompt_user)
+    new_prefix = await client.wait_for('message')
+    if new_prefix.content in allowed_prefix:
+        return new_prefix.content
+    else:
+        return PREFIX
+
 
 if __name__ == '__main__':
     client.run(TOKEN)
